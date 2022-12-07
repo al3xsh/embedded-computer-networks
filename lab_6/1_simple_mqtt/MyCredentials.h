633ad7f6b751311a88b214f765a369f7148e6b1b
Alex Shenfield <a.shenfield@shu.ac.uk>
2022-12-07 19:01:42 +0000

    accidentally commited adafruit api keys - now revoked!

--- lab_6/1_simple_mqtt/MyCredentials.h
+++ lab_6/1_simple_mqtt/MyCredentials.h
@@ -1,8 +1,8 @@
 // my wifi credentials
-const char my_ssid[] = "NOWTV2BB7F";
-const char my_pass[] = "YPYNLYWVTR";
+const char my_ssid[] = "<your ssid>";
+const char my_pass[] = "<your password>";
 
 // my adafruit io credentials
-#define CLIENT_ID "axs_arduino_190310"
-#define USER_ID   "alexshenfield"
-#define API_KEY   "aio_mWXD78OEqC3c5aW62mudy5FXnkYW"
+#define CLIENT_ID "<some client id>"
+#define USER_ID   "<your adafruit io username>"
+#define API_KEY   "<your adafruit io api key>"
